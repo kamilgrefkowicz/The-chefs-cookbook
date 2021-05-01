a2595f5b1a620371ffc5f18d205212185f5888f6
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-05-01 17:36:25 +0200

    initial commit

---
+++ src/main/java/pl/kamil/chefscookbook/ChefsCookbookApplication.java
@@ -0,0 +1,13 @@
+package pl.kamil.chefscookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefsCookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefsCookbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kamil/chefscookbook/ChefsCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamil.chefscookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefsCookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
