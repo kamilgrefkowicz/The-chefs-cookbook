89a28fef4b2232e52576df400eee334fb4fe2ea5
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-06-02 19:17:02 +0200

    pretty single recipe pdf

--- src/main/java/pl/kamil/chefscookbook/pdf/application/PdfCreationService.java
+++ src/main/java/pl/kamil/chefscookbook/pdf/application/PdfCreationService.java
@@ -1,12 +1,19 @@
 package pl.kamil.chefscookbook.pdf.application;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.Line;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.List;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.kamil.chefscookbook.food.application.dto.ingredient.IngredientDto;
@@ -14,7 +21,10 @@ import pl.kamil.chefscookbook.food.application.dto.item.RichItem;
 import pl.kamil.chefscookbook.pdf.application.port.GenerateRecipePageUseCase;
 import pl.kamil.chefscookbook.pdf.application.port.PdfCreationUseCase;
 
+
 import java.io.*;
+
+import static com.itextpdf.kernel.pdf.PdfName.*;
 
 @Component
 @AllArgsConstructor
@@ -22,13 +32,20 @@ public class PdfCreationService implements PdfCreationUseCase {
 
     private final GenerateRecipePageUseCase generateRecipePage;
 
+    @SneakyThrows
     public ByteArrayOutputStream generatePdfForItem(RichItem item) {
 
         ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-        Document document = new Document(new PdfDocument( new PdfWriter(output)));
+        Document document = new Document(new PdfDocument(new PdfWriter(output)));
 
+        PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA, PdfEncodings.CP1250, true);
+
+        document.setFont(font);
         generateRecipePage.execute(document, item);
+
+        document.showTextAligned(new Paragraph(String.format("page %s of %s", 1, 1)), 559, 806, 1, TextAlignment.RIGHT, VerticalAlignment.TOP, 0);
+
 
 
         document.close();
--- src/main/java/pl/kamil/chefscookbook/pdf/application/PdfGenerateRecipePage.java
+++ src/main/java/pl/kamil/chefscookbook/pdf/application/PdfGenerateRecipePage.java
@@ -1,43 +1,70 @@
 package pl.kamil.chefscookbook.pdf.application;
 
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.List;
-import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.*;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 import org.springframework.stereotype.Component;
 import pl.kamil.chefscookbook.food.application.dto.ingredient.IngredientDto;
 import pl.kamil.chefscookbook.food.application.dto.item.RichItem;
+import pl.kamil.chefscookbook.food.domain.staticData.Type;
 import pl.kamil.chefscookbook.pdf.application.port.GenerateRecipePageUseCase;
+
 
 @Component
 public class PdfGenerateRecipePage implements GenerateRecipePageUseCase {
+    private final Rectangle[] COLUMNS = {
+            new Rectangle(36, 36, 500, 100),
+            new Rectangle(36, 210, 254, 706),
+            new Rectangle(305, 210, 254, 706)
+    };
+    private final float[] columnWidths = {254F, 254F};
+
     @Override
     public void execute(Document document, RichItem item) {
-        generateItemNameRow(item, document);
-        generateYieldRow(item, document);
-        generateIngredientList(item, document);
 
-        generateDescriptionArea(item, document);
+
+        document.add(generateItemNameRow(item));
+        document.add(generateYieldRowForIntermediates(item));
+
+        Table table = new Table(columnWidths);
+        table.addCell(generateIngredientList(item));
+        table.addCell(generateDescriptionArea(item));
+
+        document.add(table);
+
+
     }
 
 
-    private void generateDescriptionArea(RichItem item, Document document) {
-        document.add(new Paragraph(item.getDescription()));
+    private Paragraph generateDescriptionArea(RichItem item) {
+        return new Paragraph(item.getDescription());
     }
 
-    private void generateIngredientList(RichItem item, Document document) {
+    private List generateIngredientList(RichItem item) {
         //this is an Itext list, not a Java one
         List ingredients = new List();
         item.getIngredients().forEach(ingredient ->
                 ingredients.add(getIngredientRow(ingredient)));
-        document.add(ingredients);
+        return ingredients;
     }
 
-    private void generateYieldRow(RichItem item, Document document) {
-        document.add(new Paragraph("Przepis na " + item.getRecipeYield() + " " + item.getUnit()));
+    private Paragraph generateYieldRowForIntermediates(RichItem item) {
+        if (item.getType().equals(Type.BASIC())) return new Paragraph("");
+
+        Paragraph paragraph = new Paragraph("Przepis na " + item.getRecipeYield() + " " + item.getUnit());
+        paragraph.setTextAlignment(TextAlignment.CENTER);
+
+        return paragraph;
     }
 
-    private void generateItemNameRow(RichItem item, Document document) {
-        document.add(new Paragraph(item.getName()));
+    private Paragraph generateItemNameRow(RichItem item) {
+        Paragraph paragraph = new Paragraph(item.getName());
+        paragraph.setFontSize(14);
+        paragraph.setTextAlignment(TextAlignment.CENTER);
+        return paragraph;
     }
 
     private String getIngredientRow(IngredientDto ingredient) {
