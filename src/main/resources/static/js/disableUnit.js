501adb05804642dc519b7e39a9af3596e5acf5d3
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-06-21 20:48:32 +0200

    js for create-item.html to disable unit selection if chose dish

---
+++ src/main/resources/static/js/disableUnit.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    document.getElementById('choseDish').onclick = function () {
+
+        var unitsNeeded = !this.checked;
+
+        var units = this.form.elements['unit'];
+
+        for (var i = 0, len = units.length; i < len; i++) {
+            var r = units[i];
+
+            r.disabled = !!unitsNeeded;
+
+        }
+    }
+    document.getElementById('choseIntermediate').onclick = function () {
+
+        var unitsNeeded = this.checked;
+
+        var units = this.form.elements['unit'];
+
+        for (var i = 0, len = units.length; i < len; i++) {
+            var r = units[i];
+
+            r.disabled = !!unitsNeeded;
+
+        }
+    }
+}
