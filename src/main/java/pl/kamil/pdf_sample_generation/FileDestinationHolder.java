e6a7d8e732187c59345bb781e9181df46940bcc9
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-06-27 17:56:00 +0200

    changed it so pdf generated for tests find their own file destination paths

--- src/main/java/pl/kamil/pdf_sample_generation/FileDestinationHolder.java
+++ src/main/java/pl/kamil/pdf_sample_generation/FileDestinationHolder.java
@@ -1,7 +1,9 @@
 package pl.kamil.pdf_sample_generation;
 
+import java.io.File;
+
 public class FileDestinationHolder {
 
-    public static final String PDF_SAMPLE_FOLDER = "C:\\Projekty\\cookbook-new\\src\\main\\java\\pl\\kamil\\pdf_sample_generation\\files\\";
-    public static final String PDF_TEST_FOLDER = "C:\\Projekty\\cookbook-new\\src\\test\\java\\pl\\kamil\\chefscookbook\\pdf\\temp-files\\";
+    public static final String PDF_SAMPLE_FOLDER = new File("").getAbsolutePath() + "\\src\\main\\java\\pl\\kamil\\pdf_sample_generation\\files\\";
+    public static final String PDF_TEST_FOLDER = new File("").getAbsolutePath() + "\\src\\test\\java\\pl\\kamil\\chefscookbook\\pdf\\temp-files\\";
 }
